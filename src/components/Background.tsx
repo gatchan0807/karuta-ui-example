90c70ef37c721b3c2b4dbcfdc0e5ac8b20f2f868
gatchan0807 <yuki.dolphin.koga@gmail.com>
2023-09-24 19:25:22 +0900

    feat: 元々あったApp.tsxの中身を背景コンポーネントに差し替え

---
+++ src/components/Background.tsx
@@ -0,0 +1,9 @@
+import styles from "./background.module.css";
+
+export const Background: React.FC = () => {
+	return <div className={styles.background}>
+        <div className={styles.background__font}>
+            Background
+        </div>
+    </div>;
+};
